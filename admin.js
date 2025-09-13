9da4135b97e423a15e44820782b28fbd5728b140
HARSHA164-ai <bhuvansrisaiharsha07@gmail.com>
2025-09-13 07:26:00 +0530

    please

--- admin.js
+++ admin.js
@@ -3,7 +3,7 @@ import { signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth
 import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 const logoutBtn = document.getElementById('logoutBtn');
-if (logoutBtn) logoutBtn.addEventListener('click', () => signOut(auth));
+if (logoutBtn) logoutBtn.addEventListener('click', () => window.location.href = 'index.html');
 
 // Add Company
 const companyForm = document.getElementById('companyForm');
